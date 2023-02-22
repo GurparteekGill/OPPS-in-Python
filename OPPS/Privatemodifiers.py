9446857957cffee7d26aaa522283b904a20e6961
Gurparteek Singh Gill <97549596+GurparteekGill@users.noreply.github.com>
2023-02-22 23:47:50 +0530

    Add files via upload

---
+++ OPPS/Privatemodifiers.py
@@ -0,0 +1,27 @@
+
+# Public -
+# Protected -
+# Private -
+
+class Employee:
+    __company="Duke"
+    salary=7500
+
+
+
+Gurparteek=Employee()
+Simar=Employee()
+Yuvi=Employee()
+
+
+print(Gurparteek.__company)
+#print(Simar.company)
+#print(Yuvi.company)
+
+Yuvi.salary = 11900
+Gurparteek.salary=4500
+Simar.salary=8977
+
+print(Yuvi.salary)
+print(Gurparteek.salary)
+print(Simar.salary)
